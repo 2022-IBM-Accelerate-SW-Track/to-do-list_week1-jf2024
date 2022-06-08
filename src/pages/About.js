6babed5bcd3de1c5aa46d1f334703c6f874706ac
jf2024 <josexfuentes27@gmail.com>
2022-06-08 16:50:41 -0700

    copied template provided in github

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,25 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+         <div>
+    <div class="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          // Image goes here
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Jose Xavier Fuentes</div>
+        <div className="brief_description">
+          Put details about me here
+        </div>
+      </div>
+    </div>
+  </div>
       </div>
     )
   }
